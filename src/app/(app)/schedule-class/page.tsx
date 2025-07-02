522bc0ff7e42d6f4592716bc2e3451a09347a714
Abhishek Verma <srnjp.house@gmail.com>
2025-07-02 19:06:13 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(app)/schedule-class/page.tsx
+++ src/app/(app)/schedule-class/page.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import { useState, useRef, type ChangeEvent } from "react";
+import { useState, useRef, type ChangeEvent, useEffect } from "react";
 import { useForm, Controller, type SubmitHandler } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
