22feb23ae42facc497df3e467d53d1d550f35b46
Abhishek Verma <srnjp.house@gmail.com>
2025-07-06 17:07:56 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ const ptSans = PT_Sans({
 });
 
 export const metadata: Metadata = {
-  title: 'OSO App: Learn & Deliver',
+  title: 'OSO App',
   description: 'One Student One App - Study tools and instant delivery for students in India.',
 };
 
